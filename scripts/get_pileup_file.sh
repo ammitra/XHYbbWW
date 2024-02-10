70e61bf1d93d554144ae6a0daf7481bf9ff5b67d
Amitav Mitra <amitra3@jhu.edu>
2024-02-10 15:26:10 -0600

    update to ignore old private PFNano signals in pileup

--- scripts/get_pileup_file.sh
+++ scripts/get_pileup_file.sh
@@ -1,1 +1,1 @@
-hadd -f XHYbbWWpileup.root `xrdfsls -u /store/user/ammitra/XHYbbWW/pileup/ | grep 'XHYbbWWpileup.*root' | grep -v 'backup'`
+hadd -fk XHYbbWWpileup.root `xrdfsls -u /store/user/ammitra/XHYbbWW/pileup/ | grep 'XHYbbWWpileup.*root' | grep -v 'backup' | grep -v 'MX'`
