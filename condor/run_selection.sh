651fd3e462d0f7cdd07b0d9d591e0df006d97523
Amitav Mitra <amitra3@jhu.edu>
2024-04-28 13:58:58 -0500

    fix shell script env setup, rework selection to run all systs in one job

--- condor/run_selection.sh
+++ condor/run_selection.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 echo "Run script starting"
+echo "Running on: `uname -a`"
+echo "System software: `cat /etc/redhat-release`"
+echo "Starting singularity container"
+#cmssw-el8
+echo "In singularity container"
+
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 xrdcp root://cmseos.fnal.gov//store/user/ammitra/XHYbbWW.tgz ./
-export SCRAM_ARCH=slc7_amd64_gcc820
+#export SCRAM_ARCH=slc7_amd64_gcc820
+export SCRAM_ARCH=el8_amd64_gcc10
 scramv1 project CMSSW CMSSW_12_3_5
-tar -xzvf XHYbbWW.tgz
+echo "Unpacking compiled CMSSW environment tarball..."
+tar -xzf XHYbbWW.tgz
 rm XHYbbWW.tgz
 
 mkdir tardir; cp tarball.tgz tardir/; cd tardir/
@@ -13,12 +21,23 @@ cp -r * ../CMSSW_12_3_5/src/XHYbbWW/; cd ../CMSSW_12_3_5/src/
 echo 'IN RELEASE'
 pwd
 ls
+#scramv1 b ProjectRename
+echo 'scramv1 runtime -sh'
 eval `scramv1 runtime -sh`
+echo $CMSSW_BASE "is the CMSSW we have on the local worker node"
 rm -rf timber-env
+echo 'python3 -m venv timber-env'
 python3 -m venv timber-env
+
+echo 'source timber-env/bin/activate'
 source timber-env/bin/activate
+echo "$(which python3)"
+
 cd TIMBER
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/cms.cern.ch/el8_amd64_gcc10/external/boost/1.78.0-0d68c45b1e2660f9d21f29f6d0dbe0a0/lib
+echo "STARTING TIMBER SETUP......."
 source setup.sh
+echo "FINISHED TIMBER SETUP......."
 cd ../XHYbbWW
 
 # Generate the chunking arguments
@@ -43,4 +62,3 @@ hadd -fk rootfiles/"$filename" rootfiles/*CHUNK*.root
 
 # move all final selection histos to the EOS
 xrdcp -f rootfiles/"$filename" root://cmseos.fnal.gov//store/user/ammitra/XHYbbWW/selection/
-
--- condor/run_snapshot.sh
+++ condor/run_snapshot.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 echo "Run script starting"
+echo "Running on: `uname -a`"
+echo "System software: `cat /etc/redhat-release`"
+
+# Set up pre-compiled CMSSW env
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-
-# grabs the env tarball and places it in this node environment (is that what it's called?)
 xrdcp root://cmseos.fnal.gov//store/user/ammitra/XHYbbWW.tgz ./
-
-# essentially what cmsenv does
-export SCRAM_ARCH=slc7_amd64_gcc820
+xrdcp root://cmseos.fnal.gov//store/user/ammitra/XHYbbWW.tgz ./
+export SCRAM_ARCH=el8_amd64_gcc10
 scramv1 project CMSSW CMSSW_12_3_5
-tar -xzvf XHYbbWW.tgz
-# clear up tarball and remaining junk
+echo "Unpacking compiled CMSSW environment tarball..."
+tar -xzf XHYbbWW.tgz
 rm XHYbbWW.tgz
-#rm *.root
-
 mkdir tardir; cp tarball.tgz tardir/; cd tardir/
 tar -xzf tarball.tgz; rm tarball.tgz
 cp -r * ../CMSSW_12_3_5/src/XHYbbWW/; cd ../CMSSW_12_3_5/src/
+
+# CMSREL and virtual env setup
 echo 'IN RELEASE'
 pwd
 ls
+echo 'scramv1 runtime -sh'
 eval `scramv1 runtime -sh`
+echo $CMSSW_BASE "is the CMSSW we have on the local worker node"
 rm -rf timber-env
+echo 'python3 -m venv timber-env'
 python3 -m venv timber-env
+echo 'source timber-env/bin/activate'
 source timber-env/bin/activate
+echo "$(which python3)"
+
+# Set up TIMBER
 cd TIMBER
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/cms.cern.ch/el8_amd64_gcc10/external/boost/1.78.0-0d68c45b1e2660f9d21f29f6d0dbe0a0/lib
+echo "STARTING TIMBER SETUP......."
 source setup.sh
+echo "FINISHED TIMBER SETUP......."
 cd ../XHYbbWW
 
+# MAIN FUNCTION
 echo python XHYbbWW_snapshot.py $*
 python XHYbbWW_snapshot.py $*
 
-# move all snapshots to the EOS
-xrdcp -f *.root root://cmseos.fnal.gov//store/user/ammitra/XHYbbWW/snapshots/
+# move all snapshots to the EOS (there will only be one)
+xrdcp -f HWWsnapshot*.root root://cmseos.fnal.gov//store/user/ammitra/XHYbbWW/snapshots/
